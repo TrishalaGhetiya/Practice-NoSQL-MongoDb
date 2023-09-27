8c388a3b77754dbd0f83d4c463f2c16d6f2bab22
Trishala Ghetiya <trishalaghetiya@gmail.com>
2023-09-27 22:51:36 +0530

    added product using mongoose

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const path = require('path');
+require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+//const User = require('./models/user');
 
 const app = express();
 
@@ -18,22 +19,26 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('65131dd33ddb0a774f1498f1')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('65131dd33ddb0a774f1498f1')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  console.log('database connected');
-  app.listen(3000);
-})
-
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(result => {
+    console.log('database connected');
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  })
