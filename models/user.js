81f08ff830f0f4b2e72fb795d38fcce100308e51
Trishala Ghetiya <trishalaghetiya@gmail.com>
2023-09-28 00:24:18 +0530

    orders functinality

--- models/user.js
+++ models/user.js
@@ -51,6 +51,11 @@ userSchema.methods.removeFromCart = function(productId){
   return this.save();
 }
 
+userSchema.methods.clearCart = function(productId){
+  this.cart = {items: []};
+  return this.save();
+}
+
 module.exports = mongoose.model('User', userSchema);
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
