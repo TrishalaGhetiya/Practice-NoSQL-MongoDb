6f2eb0a216efc27a066e46dd74da2fc11d1b13f3
Trishala Ghetiya <trishalaghetiya@gmail.com>
2023-09-27 16:30:41 +0530

    added cart to users

--- models/user.js
+++ models/user.js
@@ -2,9 +2,11 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class User{
-  constructor(username, email){
+  constructor(username, email, cart, _id){
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = _id;
   }
 
   save(){
@@ -18,6 +20,31 @@ class User{
     .catch(err => {
       console.log(err);
     });
+  }
+
+  addToCart(product){
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    })
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+    if(cartProductIndex >= 0){
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    }else{
+      updatedCartItems.push({productId: new mongodb.ObjectId(product._id), quantity: newQuantity})
+    }
+    
+    const updatedCart = {
+      items: updatedCartItems
+    };
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        {_id: new mongodb.ObjectId(this._id)}, 
+        {$set: {cart: updatedCart}}
+      );
   }
 
   static findById(userId){
