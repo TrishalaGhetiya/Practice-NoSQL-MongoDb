2cb96f0ab672b482d8aba648b34c6b4aa8c0cca5
Trishala Ghetiya <trishalaghetiya@gmail.com>
2023-09-27 18:46:14 +0530

    deleted products

--- models/user.js
+++ models/user.js
@@ -32,7 +32,10 @@ class User{
       newQuantity = this.cart.items[cartProductIndex].quantity + 1;
       updatedCartItems[cartProductIndex].quantity = newQuantity;
     }else{
-      updatedCartItems.push({productId: new mongodb.ObjectId(product._id), quantity: newQuantity})
+      updatedCartItems.push({
+        productId: new mongodb.ObjectId(product._id), 
+        quantity: newQuantity
+      })
     }
     
     const updatedCart = {
@@ -44,6 +47,39 @@ class User{
       .updateOne(
         {_id: new mongodb.ObjectId(this._id)}, 
         {$set: {cart: updatedCart}}
+      );
+  }
+
+  getCart(){
+    const db = getDb();
+    const productIds = this.cart.items.map(i => {
+      return i.productId;
+    });
+    return db 
+      .collection('products')
+      .find({_id: {$in: productIds}})
+      .toArray()
+      .then(products => {
+        return products.map(p => {
+          return {...p, 
+            quantity: this.cart.items.find(i => {
+            return i.productId.toString() === p._id.toString(); 
+          }).quantity
+        }
+        })
+      })
+  }
+
+  deleteItemFromCart(productId){
+    const updatedCartItems = this.cart.items.filter(item => {
+      return item.productId.toString() !== productId.toString();
+    })
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        {_id: new mongodb.ObjectId(this._id)}, 
+        {$set: {cart: {items: updatedCartItems}}}
       );
   }
 
